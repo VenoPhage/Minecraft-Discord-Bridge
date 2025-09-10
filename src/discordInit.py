5ed0cb20694664763dc7b699fba21946dae1ac5e
ExpertNugget <deaconjf04@gmail.com>
2025-09-10 07:41:01 -0600

    discord bot setup

--- src/discordInit.py
+++ src/discordInit.py
@@ -1,8 +1,18 @@
-import discord
+import discord, os
 
 
 def discordInit(token):
-    bot = discord.Bot()
+    bot = discord.Bot(intents=discord.Intents.all())
     bot.run(token)
+
+    
+    cog_list = [f[:-3] for f in os.listdir("./discordCogs") if f.endswith(".py")]
+    
+    for cog in cog_list:
+        bot.load_extension(f"discordCogs.{cog}")
+    
+    @bot.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(bot))
 
     return bot
