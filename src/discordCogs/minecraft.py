910966f9cf5d398794f4c0fff47c70518a611d0f
ExpertNugget <deaconjf04@gmail.com>
2025-09-11 08:21:28 -0600

    compressed code with functions, started setting up sftp to get messages from logs

--- src/discordCogs/minecraft.py
+++ src/discordCogs/minecraft.py
@@ -1,6 +1,8 @@
-import discord, toml
-from discord.ext import commands
+import discord, toml, pysftp, regex
+from discord.ext import commands, tasks
 from mcrcon import MCRcon
+from utils.functions import getRconConfig, getSFTPConfig
+
 
 class minecraft(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
@@ -10,20 +12,39 @@ class minecraft(commands.Cog): # create a class for our cog that inherits from c
 
     @discord.slash_command() # we can also add application commands
     async def list(self, ctx):
-        with open ('config.toml') as f:
-            config = toml.load(f)
-        rcon_ip = config['minecraft']['rcon']['host']
-        rcon_port = config['minecraft']['rcon']['port']
-        rcon_pass = config['minecraft']['rcon']['password']
-        
-        if rcon_ip == '0':
-            rcon_ip = config['minecraft']['host']
-
-
-        with MCRcon(rcon_ip, rcon_pass, rcon_port) as mcr:
+        rcon = getRconConfig()
+        with MCRcon(rcon['host'], rcon['password'], rcon['port']) as mcr:
             list = mcr.command('list')
         
+        
         await ctx.respond(list)
+
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+
+        with open ('config.toml') as f:
+            config = toml.load(f)
+        if message.channel.id != config['discord']['channel_id']:
+            return
+
+        rconConf = getRconConfig()
+        with MCRcon(rconConf['host'], rconConf['password'], rconConf['port']) as mcr:
+            mcr.command(f'tell [DISCORD]{message.author.name}: {message.content}')
+
+
+    @tasks.loop(seconds=5)
+    async def fetchLogsLoop(self):
+        sftpConf= getSFTPConfig()
+        with pysftp.Connection(sftpConf['host'], username=sftpConf['username'], password=sftpConf['password'], port=sftpConf['port']) as sftp:
+            with sftp.cd('logs') as f:
+                with f.open('latest.log', 'r') as l:
+                    for line in l:
+                        match = regex.search(r'^ ]+]: <([^>]+)> (.*)$', line)
+                        print(match)
+                        # TODO send to discord with formatting, and fix the regex
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(minecraft(bot)) # add the cog to the bot
